575a31e4f3e51484702358f7c4c72bb159729382
Ridwan <lawalridwan007gmail.com>
2026-08-16 00:33:49 +0100

    update github identity credential

--- infrastructure/terraform/identity/shopops-ci.tf
+++ infrastructure/terraform/identity/shopops-ci.tf
@@ -24,7 +24,7 @@ resource "azuread_application_federated_identity_credential" "github_main" {
   ]
 
   issuer  = "https://token.actions.githubusercontent.com"
-  subject = "repo:${var.github_owner}/${var.github_repository}:ref:refs/heads/main"
+  subject = "repo:${var.github_owner}@${var.github_owner_id}/${var.github_repository}@${var.github_repository_id}:ref:refs/heads/main"
 }
 
 resource "azurerm_role_assignment" "acr_push" {
--- infrastructure/terraform/identity/terraform.tfvars
+++ infrastructure/terraform/identity/terraform.tfvars
@@ -1,2 +1,4 @@
 github_owner      = "rid1lawal"
 github_repository = "shopops"
+github_owner_id        = "80310167"
+github_repository_id   = "1321010934"
