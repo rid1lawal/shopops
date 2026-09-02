41972d2f9fbb544e25a835cbe7d5184b17bd9ff3
Ridwan <lawalridwan007gmail.com>
2026-09-02 14:07:54 +0100

    create vpc

---
+++ infrastructure/terraform/aws/environments/dev/main.tf
@@ -0,0 +1,18 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.7.2"
+
+  name = "shopops-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["eu-west-1a", "eu-west-1b"]
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.11.0/24", "10.0.12.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    Environment = "dev"
+  }
+}
