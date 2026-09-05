66016b08da4c8a6974a75f9792db8211431961bf
Ridwan <lawalridwan007gmail.com>
2026-09-06 00:07:23 +0100

    add outputs

--- infrastructure/terraform/aws/environments/dev/outputs.tf
+++ infrastructure/terraform/aws/environments/dev/outputs.tf
@@ -9,3 +9,11 @@ output "private_subnets" {
 output "public_subnets" {
   value = module.vpc.public_subnets
 }
+
+output "cluster_name" {
+  value = module.eks.cluster_name
+}
+
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
